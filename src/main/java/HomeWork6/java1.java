4b94644751ba846e83993e4deaf07756d87fe570
MChugueva85 <livingsmiling85@gmail.com>
2023-06-28 02:59:20 +0300

    1st  commit

---
+++ src/main/java/HomeWork6/java1.java
@@ -0,0 +1,13 @@
+package HomeWork6;
+
+public class java1 {
+
+    public static void main (String[] args) {
+        int a = 2, i=0;
+        while (i<30) {
+            a=a*2;
+            i++;
+        }
+        System.out.println("Переполненность: " + a);
+    }
+}
